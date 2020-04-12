8a4e088996287c377cf4ac3a674ece7465cc8aa8
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-04-12 11:16:22 -0600

    step 10 getting started

---
+++ weatherman/src/pages/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Page1 from '../Page1/page1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <nav>
+          <Route path='/' render={(props) => (
+            <Page1 {...props}/>
+          )} />
+        </nav>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ weatherman/src/pages/Page1/page1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page1 = (props) => (
+    <div>
+        <Link to='/'>HOME</Link>
+        <p>Working</p>
+    </div>
+);
+
+export default Page1;
