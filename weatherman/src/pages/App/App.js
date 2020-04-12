395da071ff4a6af8f8cae3ac87fed7af077cd672
Andrew Clark <andrewscomputer@Andrews-MacBook-Air.local>
2020-04-12 16:03:41 -0600

    step 11: auth problems

--- weatherman/src/pages/App/App.js
+++ weatherman/src/pages/App/App.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import Page1 from '../Page1/page1';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import tokenService from '../../utils/tokenService';
+import NavBar from '../../components/NavBar/NavBar';
 
-function App() {
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser()
+  };
+}
+
+handleLogout = () => {
+  userService.logout();
+  this.setState({ user: null });
+}
+
+handleSignupOrLogin = () => {
+  this.setState({user: userService.getUser()});
+}
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
       <nav>
         <Switch>
           <Route path='/' render={(props) => (
-            <Page1 {...props}/>
+            <NavBar 
+            handleLogout={this.handleLogout}
+            user={this.state.user}
+            />
           )} />
-        </Switch>
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+          </Switch>
         </nav>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -29,7 +68,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- weatherman/src/pages/Page1/page1.js
+++ weatherman/src/pages/Page1/page1.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Page1 = (props) => (
     <div>
         <Link to='/'>HOME</Link>
-        <p>Working</p>
     </div>
 );
 
